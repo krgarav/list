bb1d7640353c773fe211b6bd95b3889f9e064139
krgarav <krgarav@gmail.com>
2023-02-13 18:49:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("hello")
+console.log("hi")
+const header= document.getElementById("main-header")
+header.style.border="2px solid black"
+console.log(typeof(header))
+const addItem = document.getElementsByClassName("title")
+addItem[0].style.fontWeight="Bold"
+addItem[0].style.color="#008000"
+
+console.log()
