b6c85e2335fe28c39f9680918f0b9e746c4b1fd6
krgarav <krgarav@gmail.com>
2023-02-14 02:51:32 +0530

    changed app.js

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ console.log("hello")
 console.log("hi")
 const header= document.getElementById("main-header")
 header.style.border="2px solid black"
-console.log(typeof(header))
 const addItem = document.getElementsByClassName("title")
 addItem[0].style.fontWeight="Bold"
 addItem[0].style.color="#008000"
-
-console.log()
+const items= document.getElementsByClassName("list-group-item")
+items[2].style.backgroundColor="#008000"
+for(let i=0 ;i<items.length;i++){
+    items[i].style.fontWeight="bold"
+}
