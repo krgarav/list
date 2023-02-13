0f3951c2f56771ad07ed5d60f48c1947d7bb4f36
krgarav <krgarav@gmail.com>
2023-02-14 03:03:13 +0530

    changes in app.js to tagname

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ header.style.border="2px solid black"
 const addItem = document.getElementsByClassName("title")
 addItem[0].style.fontWeight="Bold"
 addItem[0].style.color="#008000"
-const items= document.getElementsByClassName("list-group-item")
-items[2].style.backgroundColor="#008000"
-for(let i=0 ;i<items.length;i++){
-    items[i].style.fontWeight="bold"
+// const items= document.getElementsByClassName("list-group-item")
+// items[2].style.backgroundColor="#008000"
+// for(let i=0 ;i<items.length;i++){
+//     items[i].style.fontWeight="bold"
+// }
+const li = document.getElementsByTagName("li")
+li[2].style.backgroundColor="#008000"
+for(let i=0 ;i<li.length;i++){
+    li[i].style.fontWeight="bold"
 }
